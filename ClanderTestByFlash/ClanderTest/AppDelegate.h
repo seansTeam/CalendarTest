0ca4cf63b8d21891fc48e3ce17ee9491bc993fe0
Flash publish a new version <tasb00428@tasb00428.home>
2014-08-08 01:37:03 +0800

    Post
    Post a new folder

---
+++ ClanderTestByFlash/ClanderTest/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ClanderTest
+//
+//  Created by tasb00428 on 8/8/14.
+//  Copyright (c) 2014 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ClanderTestByFlash/ClanderTest/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  ClanderTest
+//
+//  Created by tasb00428 on 8/8/14.
+//  Copyright (c) 2014 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
